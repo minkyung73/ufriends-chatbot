6d8b1f5793522785616679e5e6ee48e09a68a742
minkyung <minkyung7365@gmail.com>
2023-02-16 14:04:33 +0900

    CharDataset 클래스에 keywords 추가

---
+++ Workspace/3-KoGPT_train_torch.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+import argparse
+import logging
+
+import numpy as np
+import pandas as pd
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.core.lightning import LightningModule
+from torch.utils.data import DataLoader, Dataset
+from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
+from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
+
+parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
+
+parser.add_argument('--chat',
+                    action='store_true',
+                    default=False,
+                    help='response generation on given user input')
+
+parser.add_argument('--sentiment',
+                    type=str,
+                    default='0',
+                    help='분노 0 / 슬픔 1 / 불안 2 / 상처 3 / 당황 4 / 기쁨 5')
+
+parser.add_argument('--keywords',
+                    type=str,
+                    default='0',
+                    help='# 대인관계, 대인관계(부부, 자녀) - 0 '
+                         '진로, 취업, 직장, 업무 스트레스 - 1'
+                         '연애, 결혼, 출산 - 2'
+                         '건강, 죽음 - 3'
+                         '재정, 은퇴, 노후준비 - 4'
+                         '학업 및 진로 - 5'
+                         '학교폭력, 따돌림 - 6'
+                         '가족관계 - 7')
+
+parser.add_argument('--model_params',
+                    type=str,
+                    default='model_chp/model_-last.ckpt',
+                    help='model binary for starting chat')
+
+parser.add_argument('--train',
+                    action='store_true',
+                    default=False,
+                    help='for training')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# token
+U_TKN = '<usr>'
+S_TKN = '<sys>'
+BOS = '</s>'        # 문장 시작 token
+EOS = '</s>'        # 문장 끝 token
+MASK = '<unused0>'
+SENT = '<unused1>'  # major_emotions
+KEY = '<unused2>'   # keywords
+PAD = '<pad>'       # 동일한 batch 내에서 입력의 크기를 동일하게 하기 위해서 사용하는 token
+# unk_token         # 모르는 단어를 나타내는 token
+
+TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
+            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
+            pad_token=PAD, mask_token=MASK)
+
+
+class CharDataset(Dataset):
+    # 데이터셋의 전처리를 해주는 부분
+    def __init__(self, chats, max_len=32):
+        self._data = chats
+        self.first = True
+        self.q_token = U_TKN
+        self.a_token = S_TKN
+        self.sent_token = SENT
+        self.key_token = KEY
+        self.bos = BOS
+        self.eos = EOS
+        self.mask = MASK
+        self.pad = PAD
+        self.max_len = max_len
+        self.tokenizer = TOKENIZER
+
+    # chatbot data의 길이를 리턴
+    def __len__(self):
+        return len(self._data)
+
+    # 로드한 챗봇 데이터를 차례차례 DataLoader로 넘겨주는 메소드
+    def __getitem__(self, idx):
+        turn = self._data.iloc[idx]
+        q = turn['user1']
+        a = turn['chatbot1']
+        sentiment = str(turn['major_emotions'])
+        keywords = str(turn['keywords'])
+
+        # 순서 변경
+        # 박사님 피드백 - 라벨이 앞에 오도록
+
+        # <U_TKN>user1발화\<SENT>major_emotions라벨\<KEY>keywords라벨
+        # 여기서 라벨들을 버트 모델에서 라벨링한 값으로 넣어줘야 하나?
+        q_toked = self.tokenizer.tokenize(self.q_token + q + \
+                                          self.sent_token + sentiment + \
+                                          self.key_token + keywords)
+        q_len = len(q_toked)
+
+        # <sys>chatbot1발화<eos>
+        a_toked = self.tokenizer.tokenize(self.a_token + a + self.eos)
+        a_len = len(a_toked)
+
+        # 질문의 길이 + 답변의 길이가 최대 길이보다 크면
+        if q_len + a_len > self.max_len:
+            a_len = self.max_len - q_len    # 답변의 길이를 (최대길이 - 질문길이)
+            if a_len <= 0:  # 질문의 길이가 너무 길어 질문만으로 최대 길이를 초과한다면
+                q_toked = q_toked[-(int(self.max_len/2)):]  # 질문길이를 최대길이의 반으로
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len    # 답변의 길이를 (최대길이 - 질문길이)
+                assert a_len > 0    # True가 아니면 AssertError 발생
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+            assert a_len == len(a_toked), f'{a_len} ==? {len(a_toked)}'
+
+        # 답변 labels = [mask, mask, ...., mask, ..., <bos>,..답변.. <eos>, <pad>....]
+        labels = [
+            self.mask,
+        ] * q_len + a_toked[1:]
+
+        # mask = 질문길이 0 + 답변길이 1 + 나머지 0
+        mask = [0] * q_len + [1] * a_len + [0] * (self.max_len - q_len - a_len)
+        self.max_len
+
+        # logging
+        if self.first:
+            logging.info("contexts : {}".format(q))
+            logging.info("toked ctx: {}".format(q_toked))
+            logging.info("response : {}".format(a))
+            logging.info("toked response : {}".format(a_toked))
+            logging.info('labels {}'.format(labels))
+            self.first = False
+
+
+        # 답변 labels를 index로 만든다
+        labels_ids = self.tokenizer.convert_tokens_to_ids(labels)
+        # 최대 길이만큼 PADDING
+        while len(labels_ids) < self.max_len:
+            labels_ids += [self.tokenizer.pad_token_id]
+
+        # 질문 + 답변을 index로 만든다.
+        token_ids = self.tokenizer.convert_tokens_to_ids(q_toked + a_toked)
+        # 최대 길이만큼 PADDING
+        while len(token_ids) < self.max_len:
+            token_ids += [self.tokenizer.pad_token_id]
+
+        # 질문+답변, 마스크, 답변
+        # 기존 token_ids: +질문문장+ +감정+ +답변+ +pad_token_id
+        # 변경 token_ids: +질문문장+ +감정+ +키워드+ +답변+ +pad_token_id
+        # pad_token_id는 max_len에 일치하도록 추가됨
+        # mask: 질문 q가 들어가는 곳에는 0, 답변 a가 위치한 곳에는 1, 빈 공간은 0으로 채워짐
+        # labels: 질문의 길이만큼 mask 문자 & 답변 a의 id
+        return token_ids, np.array(mask), labels_ids
+
+
+class KoGPT2Chat(LightningModule):
+    def __init__(self, hparams, **kwargs):
+        super(KoGPT2Chat, self).__init__()
+        self.hparams = hparams
+        self.neg = -1e18
+        self.kogpt2 = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+        self.loss_function = torch.nn.CrossEntropyLoss(reduction='none')
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        # add model specific args
+        parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--max-len',
+                            type=int,
+                            default=32,
+                            help='max sentence length on input (default: 32)')
+
+        parser.add_argument('--batch-size',
+                            type=int,
+                            default=96,
+                            help='batch size for training (default: 96)')
+        parser.add_argument('--lr',
+                            type=float,
+                            default=5e-5,
+                            help='The initial learning rate')
+        parser.add_argument('--warmup_ratio',
+                            type=float,
+                            default=0.1,
+                            help='warmup ratio')
+        return parser
+
+    def forward(self, inputs):
+        # (batch, seq_len, hiddens)
+        output = self.kogpt2(inputs, return_dict=True)
+        return output.logits
+
+    def training_step(self, batch, batch_idx):
+        token_ids, mask, label = batch
+        out = self(token_ids)
+        mask_3d = mask.unsqueeze(dim=2).repeat_interleave(repeats=out.shape[2], dim=2)
+        mask_out = torch.where(mask_3d == 1, out, self.neg * torch.ones_like(out))
+        loss = self.loss_function(mask_out.transpose(2, 1), label)
+        loss_avg = loss.sum() / mask.sum()
+        self.log('train_loss', loss_avg)
+        return loss_avg
+
+    def configure_optimizers(self):
+        # Prepare optimizer
+        param_optimizer = list(self.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters,
+                          lr=self.hparams.lr, correct_bias=False)
+        # warm up lr
+        num_train_steps = len(self.train_dataloader()) * self.hparams.max_epochs
+        num_warmup_steps = int(num_train_steps * self.hparams.warmup_ratio)
+        scheduler = get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=num_warmup_steps, num_training_steps=num_train_steps)
+        lr_scheduler = {'scheduler': scheduler, 'name': 'cosine_schedule_with_warmup',
+                        'monitor': 'loss', 'interval': 'step',
+                        'frequency': 1}
+        return [optimizer], [lr_scheduler]
+
+    def _collate_fn(self, batch):
+        data = [item[0] for item in batch]
+        mask = [item[1] for item in batch]
+        label = [item[2] for item in batch]
+        return torch.LongTensor(data), torch.LongTensor(mask), torch.LongTensor(label)
+
+    def train_dataloader(self):
+        data = pd.read_csv('./Dataset/Preprocessing/train_df_preprocess.csv')
+        self.train_set = CharDataset(data, max_len=self.hparams.max_len)
+        train_dataloader = DataLoader(
+            self.train_set, batch_size=self.hparams.batch_size, num_workers=2,
+            shuffle=True, collate_fn=self._collate_fn)
+        return train_dataloader
+
+    def chat(self, sent, keywords):
+        tok = TOKENIZER
+        # sent_tokens = tok.tokenize(sent)
+        # keywords_tokens = tok.tokenize(keywords)
+
+        # KoBERT 모델 추가
+
+        with torch.no_grad():
+            while 1:
+                q = input('user > ').strip()
+                if q == 'quit':
+                    break
+                a = ''
+                while 1:
+                    # keywords 추가함
+                    input_ids = torch.LongTensor(tok.encode(U_TKN + q + SENT + sent + KEY + keywords + S_TKN + a)).unsqueeze(dim=0)
+                    pred = self(input_ids)
+                    gen = tok.convert_ids_to_tokens(
+                        torch.argmax(
+                            pred,
+                            dim=-1).squeeze().numpy().tolist())[-1]
+                    if gen == EOS:
+                        break
+                    a += gen.replace('▁', ' ')
+                print("Simsimi > {}".format(a.strip()))
+
+
+parser = KoGPT2Chat.add_model_specific_args(parser)
+parser = Trainer.add_argparse_args(parser)
+args = parser.parse_args()
+logging.info(args)
+
+if __name__ == "__main__":
+    if args.train:
+        checkpoint_callback = ModelCheckpoint(
+            dirpath='model_chp',
+            filename='{epoch:02d}-{train_loss:.2f}',
+            verbose=True,
+            save_last=True,
+            monitor='train_loss',
+            mode='min',
+            prefix='model_'
+        )
+        # python train_torch.py --train --gpus 1 --max_epochs 3
+        model = KoGPT2Chat(args)
+        model.train()
+        trainer = Trainer.from_argparse_args(
+            args,
+            checkpoint_callback=checkpoint_callback, gradient_clip_val=1.0)
+        trainer.fit(model)
+        logging.info('best model path {}'.format(checkpoint_callback.best_model_path))
+    if args.chat:
+        model = KoGPT2Chat.load_from_checkpoint(args.model_params)
+        model.chat()
+
